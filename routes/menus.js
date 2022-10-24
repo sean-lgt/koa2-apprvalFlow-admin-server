7b40ec14777a2f5de1d358b5f52c2c7e0392e07a
林桂涛 <852951042@qq.com>
2022-10-24 18:06:31 +0800

    feat: 菜单列表查询功能，递归拼接树形列表

--- routes/menus.js
+++ routes/menus.js
@@ -32,5 +32,43 @@ router.post('/operate', async (ctx, next) => {
   }
 })
 
+// 菜单列表查询
+router.get('/list', async (ctx, next) => {
+  const { menuName, menuState } = ctx.request.query // 获取查询参数
+  const params = {}
+  if (menuName) params.menuName = menuName
+  if (menuState) params.menuState = menuState
+  let rootList = await Menu.find(params) || []
+  const permissionList = getTreeMenu(rootList, null, []) //递归遍历生成数结构
+  ctx.body = util.success(permissionList)
+})
+
+/**
+ * @description: 递归拼接树形列表
+ * @return {*}
+ * @param {*} rootList 源数据
+ * @param {*} id id标识
+ * @param {*} list 新数据
+ */
+const getTreeMenu = (rootList, id, list) => {
+  for (let i = 0; i < rootList.length; i++) {
+    let item = rootList[i]
+    if (String(item.parentId.slice().pop()) === String(id)) {
+      list.push(item._doc) //一级菜单
+    }
+  }
+  list.map(item => {
+    item.children = []
+    getTreeMenu(rootList, item._id, item.children) //递归遍历
+    if (item.children.length == 0) {
+      delete item.children
+    } else if (item.children.length > 0 && item.children[0].menuType == 2) {
+      // 快去区分按钮和菜单，用户后期做菜单按钮权限控制
+      item.action = item.children
+    }
+  })
+  return list
+}
+
 
 module.exports = router
